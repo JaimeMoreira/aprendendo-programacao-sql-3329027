d9196893ae8d8e96891fd065afaf0706dbbb31b3
Jaime Moreira <127636282+JaimeMoreira@users.noreply.github.com>
2024-04-08 23:37:09 +0000

    Resolutions CRUD

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,11 @@
 -- Insira 3 classes salariais na tabela salario_classe
-
+INSERT INTO salario_classe(id_salario,salario, nivel)
+VALUES
+(1, 2300, 'estagiário'),
+(2, 3400, 'desenvolvedor jr'),
+(3, 4600, 'desenvolvedor pleno');
 
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+INSERT INTO salario_classe(salario, nivel)
+VALUES
+(6200, 'desenvolvedor senior');
--- exercicios/1-CRUD/4-consultar-dados.sql
+++ exercicios/1-CRUD/4-consultar-dados.sql
@@ -1,4 +1,13 @@
 -- Crie uma consulta que retorne apenas as colunas "Name", "Composer" e "AlbumId" da tabela "tracks"
-
+SELECT
+name, composer, AlbumID
+FROM
+tracks;
 
 -- Repita a consulta anterior e dessa vez filtre os registros para que retornem apenas as tracks com o atributo "AlbumId" igual a 1
+SELECT
+name, composer, AlbumID, GenreId
+FROM
+tracks
+WHERE
+AlbumId = 1;
