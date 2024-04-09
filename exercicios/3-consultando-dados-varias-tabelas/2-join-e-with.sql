904aba9a3db391a28484b62711972d11cc0620ad
Jaime Moreira <127636282+JaimeMoreira@users.noreply.github.com>
2024-04-09 16:47:44 +0000

    Final Results

--- exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
+++ exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
@@ -1,4 +1,33 @@
 -- Crie uma consulta para realizar inner join com as tabelas "tracks", "albums" e "artists"
-
+SELECT
+tra.TrackId as id,
+tra.Name as musica,
+alb.title as album,
+art.Name as artista
+FROM
+tracks as tra
+INNER JOIN albums as alb ON tra.AlbumId = alb.AlbumId
+INNER JOIN artists as art ON art.ArtistId = alb.ArtistId
+WHERE
+art.name LIKE 'Gilberto%';
 
 -- Refatore a consulta anterior usando a cláusula "with" e verifique o total de músicas existente na base de terminado artista, ex: Caetano Veloso
+WITH musicas as(
+SELECT
+tra.TrackId as id,
+tra.Name as musica,
+alb.title as album,
+art.Name as artista
+FROM
+tracks as tra
+INNER JOIN albums as alb ON tra.AlbumId = alb.AlbumId
+INNER JOIN artists as art ON art.ArtistId = alb.ArtistId
+)
+
+SELECT
+artista,
+COUNT(musica) as qtd_total_musicas
+FROM musicas
+WHERE
+artista LIKE 'Caetano%';
+
